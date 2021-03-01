25cbe20f0e201210b4fe2bb91ef407378616733f
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-01 11:42:57 +0100

    Added GlobalStyle component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from "./images/logo.svg";
 import Home from "./pages/home";
+import GlobalStyle from "./styles/Global";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Home />
     </div>
   );
