3a86a27ee5f1cf8cf14c82d0b1a0ec16869abc42
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-01 11:29:27 +0100

    Added global styles and color theme

---
+++ src/styles/Global.js
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from "styled-components";
+import "typeface-archivo-narrow";
+import "typeface-merriweather";
+import Theme from "./Theme";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+}
+
+html {font-size: 100%;} /*16px*/
+
+body {
+  font-family: 'Archivo Narrow', sans-serif;
+  font-weight: 400;
+  line-height: 1.5;
+  color: ${Theme.color.mainBlack};
+}
+
+p, a, li, span, small {
+  font-family: 'Archivo Narrow', sans-serif;
+}
+
+/* values from https://type-scale.com/ */
+h1, h2, h3, h4, h5 {
+  margin: 3rem 0 1.38rem;
+  font-family: 'Merriweather', serif;
+  font-weight: 700;
+  line-height: 1.3;
+}
+
+h1 {
+  margin-top: 0;
+  font-size: 4.209rem;
+}
+
+h2 {font-size: 3.157rem;}
+
+h3 {font-size: 2.369rem;}
+
+h4 {font-size: 1.777rem;}
+
+h5 {font-size: 1.333rem;}
+
+
+a:hover {
+  text-decoration: none;
+}
+`;
+
+export default GlobalStyle;
---
+++ src/styles/Theme.js
@@ -0,0 +1,12 @@
+const Theme = {
+  // Coolors palette: https://coolors.co/353535-3c6e71-ffffff-d9d9d9-284b63
+  color: {
+    mainBlack: "#353535",
+    secondary: "3C6E71",
+    mainWhite: "#FFFFFF",
+    mainGrey: "D9D9D9",
+    primary: "#284B63",
+  },
+};
+
+export default Theme;
