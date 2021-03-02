ad5306bbc01d44e6d049a292efa7eb7c78cc6a8d
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-02 17:03:42 +0100

    Extended color palette

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,11 +1,24 @@
 const Theme = {
   // Coolors palette: https://coolors.co/353535-3c6e71-ffffff-d9d9d9-284b63
   color: {
+    // Main colors
     mainBlack: "#353535",
-    secondary: "3C6E71",
+    mainGrey: "#D9D9D9",
     mainWhite: "#FFFFFF",
-    mainGrey: "D9D9D9",
     primary: "#284B63",
+    secondary: "#3C6E71",
+
+    // Variations
+    mainBlack_dark: "#141414",
+    mainGrey_light: "#F5F5F5",
+    primary_light: "#417A9F",
+    secondary_light: "#6CAEB2",
+  },
+
+  shadow: {
+    light: "box-shadow: 0px 0px 15px 0px rgba(163,163,163,1);",
+    dark: "box-shadow: 0px 0px 15px 0px rgba(120,120,120,1);",
+    darkest: "box-shadow: 0px 0px 15px 0px rgba(46,46,46,1);",
   },
 };
 
