6ed00eaf101a60384564340bd1b2827926b19def
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-02 22:51:11 +0100

    Adjusted color palette

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,18 +1,29 @@
 const Theme = {
-  // Coolors palette: https://coolors.co/353535-3c6e71-ffffff-d9d9d9-284b63
+  // Coolors palette:
+  // Primary/Secondary https://color.romanuke.com/tsvetovaya-palitra-1545/
+  // Grey: https://coolors.co/f8f9fa-e9ecef-dee2e6-ced4da-adb5bd-6c757d-495057-343a40-212529
   color: {
     // Main colors
-    mainBlack: "#353535",
-    mainGrey: "#D9D9D9",
-    mainWhite: "#FFFFFF",
-    primary: "#284B63",
-    secondary: "#3C6E71",
+    mainBlack: "#100606",
+    primary: "#547b01",
+    primary_dark: "#304C02",
+    primary_light: "#90A900",
+    secondary: "#B9C21D",
 
-    // Variations
-    mainBlack_dark: "#141414",
-    mainGrey_light: "#F5F5F5",
-    primary_light: "#417A9F",
-    secondary_light: "#6CAEB2",
+    grey_9: "#212529",
+    grey_8: "#343A40",
+    grey_7: "#495057",
+    grey_6: "#6C757D",
+    grey_5: "#ADB5BD",
+    grey_4: "#CED4DA",
+    grey_3: "#DEE2E6",
+    grey_2: "#E9ECEF",
+    grey_1: "#F8F9FA",
+
+    mainGrey: "#CED4DA",
+    mainWhite: "#FFFFFF",
+
+    // Not used yet
   },
 
   shadow: {
