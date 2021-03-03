99c5f924b03410d627f9306dac0bd6293e79973c
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-04 00:36:46 +0100

    About section added

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,22 @@
+import { FaSmile } from "react-icons/fa";
+
+export const HomeObjOne = {
+  colorBg: "black",
+  imgStart: false,
+  start: false,
+
+  headline: "About me",
+  subtitle: "",
+  description: `Hi there, thanks for your visit on my website.
+   
+  My name is Wendelin Steiner and I enjoy hiking, nature, photography and drones. So why not combine altogether? This and the current Covid situation \u{1F605} was the birth of the idea behind Wanderruf (German word for "call to wander"). 
+  The idea to explore Switzerland, to capture the beauty of the nature and to share it. 
+  I would be delighted if my pictures and videos serve as inspiration for your next adventure.
+  
+  Don't hesitate to contact me for a collaboration or with suggestions for places worth exploring.
+  `,
+  buttonLabel: "Contact",
+
+  img: require("../../images/profile.jpeg").default,
+  alt: "Image of website owner",
+};
