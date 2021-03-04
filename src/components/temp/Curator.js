2c42231d73431322038884808e43f5ad73d2df35
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-04 11:19:46 +0100

    Added Curator component for testing

---
+++ src/components/temp/Curator.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+
+const Curator = () => {
+  return (
+    <div>
+      <div id="curator-feed-default-feed-layout">
+        <a href="https://curator.io" target="_blank" class="crt-logo crt-tag">
+          Powered by Curator.io
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Curator;
