904160752d7ae4d6be2fb352e93c17a94a4dd938
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-04 22:22:34 +0100

    Added styling to Contact Form

--- src/components/Form/Form.element.js
+++ src/components/Form/Form.element.js
@@ -1,6 +1,41 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const FormWrapper = styled.form``;
-export const FormGroup = styled.div``;
-export const FormInput = styled.input``;
-export const FormTextarea = styled.textarea``;
+import Theme from "./../../styles/Theme";
+
+export const FormWrapper = styled.form`
+  width: 100%;
+  max-width: 35rem;
+  box-shadow: ${Theme.shadow.light};
+`;
+
+export const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const baseInputStyles = css`
+  background: ${Theme.color.grey_1};
+  color: ${Theme.color.mainBlack};
+  padding: 0.75rem 1rem;
+  width: 100%;
+  border-radius: ${Theme.radius};
+  outline: none;
+  border: none;
+  margin-bottom: 1.25rem;
+  letter-spacing: ${Theme.spacing};
+
+  &::placeholder {
+    color: ${Theme.color.grey_5};
+    letter-spacing: ${Theme.spacing};
+  }
+`;
+
+export const FormInput = styled.input`
+  ${baseInputStyles};
+`;
+
+export const FormTextarea = styled.textarea`
+  ${baseInputStyles};
+  resize: none;
+`;
