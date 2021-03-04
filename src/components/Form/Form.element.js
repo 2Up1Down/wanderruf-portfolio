ebb7d3da45e2489b3b64c66f6d644b9884cc8e33
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-04 16:05:40 +0100

    Created form structure (without styling)

---
+++ src/components/Form/Form.element.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const FormWrapper = styled.form``;
+export const FormGroup = styled.div``;
+export const FormInput = styled.input``;
+export const FormTextarea = styled.textarea``;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Button from "../globals/Button";
+import {
+  FormGroup,
+  FormInput,
+  FormTextarea,
+  FormWrapper,
+} from "./Form.element";
+
+const Form = ({ action = "#", method = "POST" }) => {
+  return (
+    <FormWrapper action={action} method={method}>
+      <FormGroup>
+        <FormInput name="name" placeholder="Name" type="text" />
+        <FormInput name="email" placeholder="Email" type="email" />
+        <FormTextarea name="message" placeholder="Message" rows="5" />
+        <Button type="submit">Submit here</Button>
+      </FormGroup>
+    </FormWrapper>
+  );
+};
+
+export default Form;
