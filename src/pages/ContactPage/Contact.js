83efc431f69a26cf3a88525be48e825369fac9dc
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-04 19:47:24 +0100

    Moved Contact Page

---
+++ src/pages/ContactPage/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import Hero from "./../../components";
+import { Form } from "./../../components";
+
+const Contact = () => {
+  return (
+    <>
+      hello from contact
+      <h1>hello from contact</h1>
+      {/* <Hero /> */}
+      <Form action="https://formspree.io/f/xjvpzqzz" method="POST"></Form>
+    </>
+  );
+};
+
+export default Contact;
