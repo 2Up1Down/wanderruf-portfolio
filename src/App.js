e3f6f510abcd218cb9bb67888d09ea77acea8d10
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-04 22:23:11 +0100

    Added contact route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import GlobalStyle from "./styles/Global";
 import { Footer, Navbar } from "./components";
 import Home from "./pages/HomePage/Home";
+import Contact from "./pages/ContactPage/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/contact" component={Contact} />
       </Switch>
       <Footer />
     </Router>
