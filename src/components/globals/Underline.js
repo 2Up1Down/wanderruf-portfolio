48eaeb4993f424ac783ebc7801c40cbe3a13c5a0
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-04 10:28:51 +0100

    Underline component added

---
+++ src/components/globals/Underline.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import Theme from "./../../styles/Theme";
+
+export const Underline = styled.div`
+  width: 5rem;
+  height: 0.25rem;
+  margin-bottom: 2rem;
+  background: ${Theme.color.secondary};
+`;
