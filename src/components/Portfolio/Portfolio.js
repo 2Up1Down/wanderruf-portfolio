bdbb0a482b76bb39da1d72806f1a297651f171b0
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-05 17:58:45 +0100

    Added Curator Feed to Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,12 +3,16 @@ import JuicerFeed from "react-juicer-feed";
 
 import { Heading } from "./../globals/Heading";
 import { PortfolioWrapper, PortfolioFeed } from "./Portfolio.element";
+import { CuratorFeed } from "..";
 
 const Portfolio = () => {
   return (
     <PortfolioWrapper id="portfolio">
       <Heading title="Portfolio" position="center" color="white" />
       <PortfolioFeed>
+        Curatorfeed:
+        <CuratorFeed />
+        Juicerfeed:
         <JuicerFeed feedId="wanderruf" />
       </PortfolioFeed>
     </PortfolioWrapper>
