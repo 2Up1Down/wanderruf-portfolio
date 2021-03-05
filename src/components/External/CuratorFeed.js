f4c92f4c488b4c81cd9867a5ef03b6853ac6f97f
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-05 17:57:05 +0100

    First test with useState and useEffect

--- src/components/External/CuratorFeed.js
+++ src/components/External/CuratorFeed.js
@@ -1,13 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { CuratorWrapper } from "./CuratorFeed.elements";
+import Button from "./../globals/Button";
 
 const CuratorFeed = () => {
+  const [count, setCount] = useState(0);
+
+  // const data = {};
+
+  useEffect(async () => {
+    document.title = `You clicked ${count} times on the button`;
+  });
+
   return (
     <CuratorWrapper>
       <div id="curator-feed-default-feed-layout">
         <a href="https://curator.io" target="_blank" class="crt-logo crt-tag">
           Powered by Curator.io
         </a>
+        <h2>{count}</h2>
+        <Button onClick={() => setCount(count + 1)}>Click me</Button>
       </div>
     </CuratorWrapper>
   );
