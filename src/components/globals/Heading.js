f2eef41f7f52bd5c89bc9942cb72daf1ae304518
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-05 14:24:04 +0100

    Refactoring of Header component

--- src/components/globals/Heading.js
+++ src/components/globals/Heading.js
@@ -2,14 +2,31 @@ import React from "react";
 import styled from "styled-components";
 
 import { Underline } from "./Underline";
+import Theme from "./../../styles/Theme";
+
+const HeadingWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: ${(props) => {
+    if (props.position === "left") return "flex-start";
+    if (props.position === "center") return "center";
+    if (props.position === "right") return "flex-end";
+    return "flex-start";
+  }};
+  color: ${(props) => {
+    if (props.color === "white") return Theme.color.mainWhite;
+    if (props.color === "black") return Theme.color.mainBlack;
+    return Theme.color.mainBlack;
+  }};
+`;
 
 const Title = styled.h2``;
 
-export const Heading = ({ title }) => {
+export const Heading = ({ color, position, title = "" }) => {
   return (
-    <>
+    <HeadingWrapper color={color} position={position}>
       <Title>{title}</Title>
       <Underline />
-    </>
+    </HeadingWrapper>
   );
 };
