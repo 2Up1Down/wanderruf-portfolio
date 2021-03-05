d95aa32a96ae18e47210f58e15ed75f5a7c303f4
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-05 16:23:31 +0100

    Added Curator Styling

---
+++ src/components/External/Curator.elements.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import Theme from "./../../styles/Theme";
+
+export const CuratorWrapper = styled.section`
+  background: ${Theme.color.mainBlack};
+`;
--- src/components/External/Curator.js
+++ src/components/External/Curator.js
@@ -1,14 +1,15 @@
 import React, { useEffect } from "react";
+import { CuratorWrapper } from "./Curator.elements";
 
 const Curator = () => {
   return (
-    <div>
+    <CuratorWrapper>
       <div id="curator-feed-default-feed-layout">
         <a href="https://curator.io" target="_blank" class="crt-logo crt-tag">
           Powered by Curator.io
         </a>
       </div>
-    </div>
+    </CuratorWrapper>
   );
 };
 
