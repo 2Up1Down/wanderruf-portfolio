3a59a62d888e08afd5de231b3db40cf780ff6675
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-05 16:12:45 +0100

    Added Contact component to contact page

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,14 +1,10 @@
 import React from "react";
-// import Hero from "./../../components";
-import { Form } from "../../components";
+import { Contact } from "../../components";
 
 const ContactPage = () => {
   return (
     <>
-      hello from contact
-      <h1>hello from contact</h1>
-      {/* <Hero /> */}
-      <Form action="https://formspree.io/f/xjvpzqzz" method="POST"></Form>
+      <Contact />
     </>
   );
 };
