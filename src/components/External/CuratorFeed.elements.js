2e282531b71e5cf85ee05bba960b07d42de7ca83
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-07 19:51:05 +0100

    Added styling to curator link

--- src/components/External/CuratorFeed.elements.js
+++ src/components/External/CuratorFeed.elements.js
@@ -5,6 +5,8 @@ export const CuratorWrapper = styled.section`
   background: ${Theme.color.mainBlack};
 `;
 
-export const CuratorFeedLayout = styled.div``;
-
-export const CuratorLink = styled.a``;
+export const CuratorFeedLayout = styled.div`
+  a {
+    color: ${Theme.color.mainWhite};
+  }
+`;
