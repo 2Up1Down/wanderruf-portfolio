1e8c4c8db92f95472153125cac9b91857c65f58a
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-07 19:19:17 +0100

    Add Curator hook

---
+++ src/hooks/useCurator.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+export default function useCurator() {
+  useEffect(() => {
+    async function getFeed() {
+      var i,
+        e,
+        d = document,
+        s = "script";
+      i = d.createElement("script");
+      i.async = 1;
+      i.src =
+        "https://cdn.curator.io/published/b1648a76-9b37-4004-a47a-9b89c97d686e.js";
+      e = d.getElementsByTagName(s)[0];
+      e.parentNode.insertBefore(i, e);
+    }
+
+    getFeed();
+  });
+}
