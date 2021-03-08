f4444157f2b7e155478a8040bd14a7ee29539ce2
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-08 11:57:49 +0100

    Got rid of unused components

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { Helmet } from "react-helmet";
 
 import { Hero, SocialMedia } from "../../components";
-// import Player from "../../components/temp/Player";
 import InfoSection from "../../components/InfoSection/InfoSection";
 import MetaDecorator from "../../components/SEO/MetaDecorator";
 import { HomeObjOne, SocialMediaData, MetaData } from "./Data";
