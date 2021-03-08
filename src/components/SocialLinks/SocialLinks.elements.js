b1eb6dbd8b4d40f23d233745c674638d94345142
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-08 10:29:12 +0100

    Gap currently not supported by safari, workaround implemented

--- src/components/SocialLinks/SocialLinks.elements.js
+++ src/components/SocialLinks/SocialLinks.elements.js
@@ -4,17 +4,20 @@ import Theme from "../../styles/Theme";
 import { setTransition } from "../../styles/Setters";
 import { media } from "./../../styles/Setters";
 
-export const SocialLinksWrapper = styled.div``;
+export const SocialLinksWrapper = styled.div`
+  width: 10rem;
+`;
 
 export const SocialList = styled.ul`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  gap: 2.5rem;
 
+  // gap currently not working on Safari Browser
+  /* gap: 2.5rem;
   ${media.md`
     gap: 1.25rem;
-  `};
+  `}; */
 `;
 
 export const SocialItem = styled.li`
