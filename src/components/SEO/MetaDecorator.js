1153ef5e60d3be5c28ae216ef36920dbf11e9644
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-03-09 12:42:46 +0100

    Modifications to OpenGraph meta tags

--- src/components/SEO/MetaDecorator.js
+++ src/components/SEO/MetaDecorator.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 
-const MetaDecorator = ({ title, description, author, img }) => {
+const MetaDecorator = ({ title, description, author, img, url }) => {
   return (
     <Helmet>
       {/* /* General tags */}
@@ -10,15 +10,16 @@ const MetaDecorator = ({ title, description, author, img }) => {
       <meta name="description" content={description} />
       <meta name="author" content={author} />
       {/* /* OpenGraph tags */}
-      <meta name="og:title" content={title} />
-      <meta name="og:description" content={description} />
-      <meta name="og:image" content={img} />
-      <meta name="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={img} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url}></meta>
       {/* Twitter Card tags */}
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={img} />
-      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:card" content="summary_large_image" />
     </Helmet>
   );
 };
